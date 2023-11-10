229056b7d9c6604169dc38db7b6c1a7f14bf033d
malin <malin.rosen@medieinstitutet.se>
2023-11-10 17:54:24 +0100

    nameexercise

--- src/js/main.js
+++ src/js/main.js
@@ -18,8 +18,21 @@ import './../css/style.css'
 
 // }
 
+const myName = ["M", "A", "L", "I", "N"];
+const lettersContiner = document.getElementById("myName");
+
+myName.forEach((value) => {
+  const nameContainer = document.createElement("div");
+  const name = document.createElement("h2");
+
+  name.innerHTML = value;
+  nameContainer.className = "letters";
 
 
+  nameContainer.appendChild(name);
+  lettersContiner.appendChild(nameContainer);
 
+
+})
 
 
