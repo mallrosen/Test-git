4c96d23cff93e2b7897dee20773032430c5820c1
malin <malin.rosen@medieinstitutet.se>
2023-11-10 17:27:40 +0100

    hide the function

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,25 @@
 import './../css/style.css'
 
-const theNumber = 53;
-let theGuess =0;
+// const theNumber = 53;
+// let theGuess =0;
 
-while (theGuess != theNumber) {
-  theGuess = +prompt("Gissa talet mellan 0-100");
+// while (theGuess != theNumber) {
+//   theGuess = +prompt("Gissa talet mellan 0-100");
 
-  if (theGuess > theNumber){
-    alert("Du gissade för högt");
-  }
-  
-  else if (theGuess == theNumber){
-    alert("Du gissade rätt");
+//   if (theGuess > theNumber){
+//     alert("Du gissade för högt");
+//   }
 
-  }
+//   else if (theGuess == theNumber){
+//     alert("Du gissade rätt");
+
+//   }
 
 
-}
+// }
+
+
+
 
 
 
